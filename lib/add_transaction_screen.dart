09187f3625edc4f1747ef34a068f511b7bb0ff56
khkr <32921476+khkred@users.noreply.github.com>
2023-11-09 19:20:00 -0800

    Database insert is working

--- lib/add_transaction_screen.dart
+++ lib/add_transaction_screen.dart
@@ -1,5 +1,6 @@
+import 'package:drift/drift.dart' as drift;
 import 'package:flutter/material.dart';
-import 'package:shop_ledger/models/transaction.dart';
+import 'package:shop_ledger/models/database.dart';
 
 
 class AddTransactionScreen extends StatefulWidget {
@@ -12,48 +13,82 @@ class AddTransactionScreen extends StatefulWidget {
 class _AddTransactionScreenState extends State<AddTransactionScreen> {
 
   final _formKey = GlobalKey<FormState>();
-  String name = '';
-  double amount = 0;
+
+  final _amountController = TextEditingController();
+  final _itemNameController = TextEditingController();
+
+
+  void _submit() async {
+    final database = AppDatabase();
+    if (_formKey.currentState!.validate()) {
+      print("AMOUNT ENTERED: ${_amountController.text}");
+      int amount = int.parse(_amountController.text);
+      if (_itemNameController.text.isEmpty) {
+        await database.into(database.transactionItems).insert(
+            TransactionItemsCompanion.insert(amount: amount));
+      } else {
+        String item = _itemNameController.text.toString();
+        await database.into(database.transactionItems).insert(
+            TransactionItemsCompanion.insert(amount: amount, item: drift.Value(item)));
+      }
+    }
+    List<TransactionItem> allItems = await database.select(
+        database.transactionItems).get();
+
+    print('Items in database: $allItems');
+  }
+
+
+  @override
+  void dispose() {
+    _amountController.dispose();
+    _itemNameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Container(
         padding: const EdgeInsets.all(16.0),
         width: double.infinity,
         child: Builder(
-          builder: (context) => Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  TextFormField(
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(labelText: 'Amount'),
-                    validator: (value) {
-                      if(value == null || value.isEmpty)
-                        {
-                          return 'Please Enter a value';
-                        }
-                    }
-                  ),
-
-                  TextFormField(
-                    decoration:  const InputDecoration(labelText: 'Item'),
-                  ),
-                  const SizedBox(height: 10,),
-                  Row(
+          builder: (context) =>
+              Form(
+                  key: _formKey,
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      ElevatedButton(onPressed: (){
-                        if(_formKey.currentState!.validate()){
+                      TextFormField(
+                          controller: _amountController,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                              labelText: 'Amount'),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please Enter a value';
+                            }
+                            return null;
+                          }
 
-                        }
-                      }, child: const Text('Submit'))
+                      ),
+
+                      TextFormField(
+                        controller: _itemNameController,
+                        decoration: const InputDecoration(labelText: 'Item'),
+                      ),
+                      const SizedBox(height: 10,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(onPressed: () {
+                            _submit();
+                          }, child: const Text('Submit'))
+                        ],
+                      ),
                     ],
-                  ),
-                ],
-              )),
+                  )),
         ),
       ),
     );
